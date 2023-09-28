120ec3402be5488f15adfdc630223571feef2294
git-saj <admin@sajbox.com>
2023-09-28 04:58:44 +0100

    updated

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,8 +1,4 @@
 return {
-  { "echasnovski/mini.pairs", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-
   -- temporarily disabled due to empty file issue
   -- { "nvim-navic", enabled = false },
 }
